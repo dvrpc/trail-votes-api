a535d090f133916147d967f8e6cace9a4a3bced9
aaronfraint <aaronfraint@icloud.com>
2022-02-01 15:11:43 -0500

    add POST route to save user votes

--- app/app/main.py
+++ app/app/main.py
@@ -1,13 +1,24 @@
 import os
 from dotenv import find_dotenv, load_dotenv
 import asyncpg
-from asyncio import sleep
 from typing import List
-from fastapi import FastAPI, Query, BackgroundTasks
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from datetime import datetime
 
-from .database import postgis_query_to_geojson, sql_query_raw
+from .database import postgis_query_to_geojson
+
+
+class NewVote(BaseModel):
+    """
+    A vote requires the user's email address
+    and a list of the trailhead IDs that were selected
+    """
+
+    email_address: str
+    trailheads: List[int]
+
 
 load_dotenv(find_dotenv())
 
@@ -27,17 +38,22 @@ app.add_middleware(
 
 
 @app.get(URL_PREFIX + "/trailheads", tags=["geojson"])
-async def trailheads():
+async def get_all_trailheads_as_geojson():
     """Get all trailhead points as geojson"""
+
     query = """
-        select gid, geom as geometry from trailheads
+        select
+            gid,
+            geom as geometry
+        from trailheads
     """
     return await postgis_query_to_geojson(query, ["gid", "geometry"], DATABASE_URL)
 
 
 @app.get(URL_PREFIX + "/trail-segments", tags=["geojson"])
-async def trail_segments():
+async def get_all_trail_segments_as_geojson():
     """Get all trail segment lines as geojson"""
+
     query = """
         select
             delcogis_5 as trail_name,
@@ -46,3 +62,26 @@ async def trail_segments():
         from trail_segments
     """
     return await postgis_query_to_geojson(query, ["trail_name", "status", "geometry"], DATABASE_URL)
+
+
+@app.post(URL_PREFIX + "/add-vote/", tags=["vote"])
+async def define_new_group_of_tazs(vote: NewVote):
+    """
+    Add a new row to the 'user_votes' table with the provided
+    email address and list of trailhead IDs.
+    """
+
+    conn = await asyncpg.connect(DATABASE_URL)
+
+    await conn.execute(
+        """
+        INSERT INTO user_votes(email_address, submitted_on, trailheads)
+        VALUES($1, $2, $3)
+    """,
+        vote.email_address,
+        datetime.now(),
+        vote.trailheads,
+    )
+    await conn.close()
+
+    return {"data": vote}
